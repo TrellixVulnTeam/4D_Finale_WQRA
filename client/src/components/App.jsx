678b8af7181fc9f8c951d604e761b5f0a59599f1
awqsomee <awqsome@bk.ru>
2022-04-23 23:09:18 +0300

    news

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,35 +7,87 @@ import Footer from './UI/footer/Footer.jsx'
 import { useDispatch, useSelector } from 'react-redux'
 import { auth } from '../actions/user.js'
 import { useEffect } from 'react'
-import Account from './account/Account'
-import { SearchContext } from '../context/index.js'
+import { getNews } from '../actions/news.js'
+// import parser from '../utils/parser.js'
+// import Parser from 'rss-parser'
 
 function App() {
   const isAuth = useSelector((state) => state.user.isAuth)
+  const [news, setNews] = useState([])
   const dispatch = useDispatch()
-  const [search, setSearch] = useState('')
-  console.log(search)
-
   useEffect(() => {
     dispatch(auth())
+    getAllNews()
   }, [])
 
+  const resources = [
+    'http://static.feed.rbc.ru/rbc/logical/footer/news.rss',
+    'https://ria.ru/export/rss2/archive/index.xml',
+  ]
+
+  const getAllNews = async () => {
+    const data = await Promise.all(
+      resources.map(async (item, index) => {
+        let response = await getNews(item)
+        const title = response.title
+        const link = response.link
+        const data = response.items
+        const items = await data.map((item, index) => {
+          return {
+            // number: index + 1,
+            title: item.title,
+            content: item.content,
+          }
+        })
+        console.log(title)
+        console.log(link)
+        console.log(items)
+        return { title, link, items }
+      })
+    )
+    console.log(data)
+    return data
+  }
+
+  news = [
+    {
+      items: [
+        {
+          title: 'В Госдепе отказались отвечать на вопрос о визите Блинкена в Киев',
+          content:
+            'Госдепартамент США отказался комментировать заявле…ретарь США Энтони Блинкен посетит Киев 24 апреля.',
+        },
+        {
+          title: 'Зеленский сообщил о визите Блинкена и Остина в Киев',
+          content:
+            'Глава Госдепа США Энтони Блинкен и глава Миноборон…о возможности их визита сообщало издание Politico',
+        },
+      ],
+      title: 'www.rbc.ru',
+      link: 'https://www.rbc.ru',
+    },
+  ]
+
   return (
-    <SearchContext.Provider
-      value={{
-        search,
-        setSearch,
-      }}
-    >
-      <BrowserRouter>
-        <div className="app">
-          <Navbar />
-          {/* {!isAuth && (
+    // <SearchContext.Provider
+    //   value={{
+    //     search,
+    //     setSearch,
+    //   }}
+    // >
+    <BrowserRouter>
+      <div className="app">
+        <Navbar />
+        {news &&
+          news.forEach((element) => {
+            element.items.map((element) => <div key={element.title}> {element.content} </div>)
+          })}
+        {/* {!isAuth && (
             <Routes> */}
-          {/* <Route path="/" element={<Navigate to="/stocks" />}></Route>
+        {/* <Route path="/" element={<Navigate to="/stocks" />}></Route>
               <Route />
               <Route path="/stocks" element={<StockList title="Каталог акций" />} /> */}
-          {/* 
+        {/* 
               <Route
                 path="*"
                 element={
@@ -48,11 +100,11 @@ function App() {
             </Routes>
           )}
           {isAuth && ( */}
-          {/* <Routes> */}
-          {/* <Route path="/" element={<Navigate to="/stocks" />}></Route> */}
-          {/* <Route /> */}
-          {/* <Route path="/stocks" element={<StockList title="Каталог акций" />} /> */}
-          {/* <Route path="/account" element={<Account />} />
+        {/* <Routes> */}
+        {/* <Route path="/" element={<Navigate to="/stocks" />}></Route> */}
+        {/* <Route /> */}
+        {/* <Route path="/stocks" element={<StockList title="Каталог акций" />} /> */}
+        {/* <Route path="/account" element={<Account />} />
               <Route
                 path="*"
                 element={
@@ -64,10 +116,10 @@ function App() {
               />
             </Routes>
           )} */}
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </SearchContext.Provider>
+        <Footer />
+      </div>
+    </BrowserRouter>
+    // </SearchContext.Provider>
   )
 }
 
